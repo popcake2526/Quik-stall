5610078ec714d0d1cf486bcc29175148685eadc2
lei <lbarrozo@outlook.ph>
2025-03-09 21:49:07 +0800

    Add test install. this is working

---
+++ test/install.ps1
@@ -0,0 +1,21 @@
+﻿# Make sure winget is available - this will force install winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Please install it first."
+    exit
+}
+
+# List of apps i want to install
+$apps = @(
+    "Google.Chrome",
+    "Mozilla.Firefox",
+    "7zip.7zip",
+    "Microsoft.PowerShell"
+)
+
+# Install every app
+foreach ($app in $apps) {
+    Write-Host "Installing $app..."
+    winget install --id=$app --silent --accept-source-agreements --accept-package-agreements
+}
+
+Write-Host "All applications have been installed!"
