c232352b68e781fb9f75e83ead918a1315841e5b
le yo <lbarrozo@outlook.ph>
2025-03-17 15:57:56 +0800

    Added work laptop apps

---
+++ laptop/install.ps1
@@ -0,0 +1,33 @@
+﻿# Make sure winget is available - this will force install winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Please install it first."
+    exit
+}
+
+# List of apps i want to install
+$apps = @(
+    "Microsoft.VisualStudioCode",
+    "Mozilla.Firefox",
+    "Microsoft.Teams",
+    "Microsoft.PowerShell"
+    "Brave.Brave",
+    "Google.Chrome",
+    "Google.ChromeRemoteDesktop",
+    "Bitwarden.Bitwarden",
+    "Notion.Notion",
+    "Git.Git",
+    "Microsoft.PowerShell.Preview",
+    "Viber.Viber",
+    "tailscale.tailscale",
+    "Microsoft.VCRedist.2015+.x64",
+    "7zip.7zip",
+    "Microsoft.SQLServerManagementStudio"
+)
+
+# Install every app
+foreach ($app in $apps) {
+    Write-Host "Installing $app..."
+    winget install --id=$app --silent --accept-source-agreements --accept-package-agreements
+}
+
+Write-Host "All applications have been installed!"
